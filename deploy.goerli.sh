df618c744c872bf07ecb6139ef5f1a6d05ffcdba
Hetman Jones <hetman.jones@protonmail.com>
2022-11-21 13:24:39 +0000

    added deployer

---
+++ deploy.goerli.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "About to deploy to Goerli" 
+
+source .env.goerli
+
+forge script script/InterestSwap.s.sol:InterestSwapScript --rpc-url $JSON_RPC_URL --private-key $PRIVATE_KEY --chain-id 1313161555 --gas-price 120000000000
--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "About to deploy"
-
-source .env
-
-forge script script/InterestSwap.s.sol:InterestSwapScript --rpc-url $GOERLI_RPC_URL --private-key $PRIVATE_KEY --broadcast
